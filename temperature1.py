329aafc044f1b9a9d0cb9d7a044bdb9f56216a7e
TanmayCodes-afk <tanmaysaini349@gmail.com>
2026-03-12 22:41:40 +0530

    Add files via upload

---
+++ temperature1.py
@@ -0,0 +1,14 @@
+#celsius_to_fahrenheit.py
+
+def convert(celsius):
+  return (celsius * 9/5) + 32
+
+#fahrenheit_to_celsius.py
+
+def convert(fahrenheit):
+ return (fahrenheit - 32) * 5/9
+
+#celsius_to_kelvin.py
+
+def convert(celsius):
+ return celsius + 273.15
